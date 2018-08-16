f421206cbc770a16e29c136d719059993465f989
aduo122 <ytarch122@gmail.com>
2018-08-16 01:44:02 -0400

    update

--- clarifai_test.py
+++ clarifai_test.py
@@ -1,18 +1,62 @@
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
+import requests
 
 
 app = ClarifaiApp(api_key='d4f76e005d404eb69893a5f721550d62')
 
-# app = ClarifaiApp()
-
 model = app.models.get('general-v1.3')
 
-URL = 'https://farm7.staticflickr.com/5769/21094803716_da3cea21b8_o.jpg'
-# response = model.predict_by_url(url=URL)
 
-image = ClImage(url=URL)
+picture_lib = 'https://s3.amazonaws.com/clarifai-data/backend/api-take-home/images.txt'
+page = requests.get(picture_lib)
+urls = page.text.split()
 
-response = model.predict([image])
+tag_index = {}
+revert_index = {}
 
-print(response)
+res = []
+count = 1
+for line in urls:
+
+    # URL = 'https://farm7.staticflickr.com/5769/21094803716_da3cea21b8_o.jpg'
+    URL = line
+    image = ClImage(url=URL)
+    response = model.predict([image])
+
+    result = response['outputs'][0]['data']['concepts']
+    # print(result)
+    tag = []
+    for n in result:
+        tag.append((n['name'], n['value']))
+        if n['name'] in revert_index:
+            revert_index[n['name']].append((n['value'], URL))
+        else:
+            revert_index[n['name']] = [(n['value'], URL)]
+    tag_index[URL] = tag
+    count += 1
+    print(count, URL)
+    if count > 10:
+        break
+
+# URL = 'https://farm7.staticflickr.com/5769/21094803716_da3cea21b8_o.jpg'
+
+
+# URL = urls[0]
+# image = ClImage(url=URL)
+# response = model.predict([image])
+#
+# result = response['outputs'][0]['data']['concepts']
+# # print(result)
+# tag = []
+# for n in result:
+#     tag.append((n['name'], n['value']))
+#     if n['name'] in revert_index:
+#         revert_index[n['name']].append((n['value'], URL))
+#     else:
+#         revert_index[n['name']] = [(n['value'], URL)]
+# tag_index[URL] = tag
+
+
+print (revert_index, tag_index)
+
